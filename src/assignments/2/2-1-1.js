9e8cad20fcb154de7bc0fbfc350c50f3de377d85
parsley001 <dubhe.kai1@gmail.com>
2026-01-22 14:23:32 +0900

    main のassignments内各週の課題ファイルを削除(自分の回答はmykadaiブランチで管理)

--- src/assignments/2/2-1-1.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-    createCanvas(200, 200);
-    background(0,0, 255);
-}
-
-function draw() {
-
-}
--- src/assignments/2/2-1-2.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-    createCanvas(600, 300);
-    background(255, 255, 0);
-}
-
-function draw() {
-
-}
